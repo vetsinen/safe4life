5984d918f4cf8a5af87c2724bb2e13f3743b5db9
vetsinen <vetsinen@gmail.com>
2020-11-08 01:36:05 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+SECRET_KEY = os.getenv("BOTTOKEN")
+import telebot
+
+bot = telebot.TeleBot(os.getenv("BOTTOKEN"))
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+bot.polling()
